0e5cdb8241c5e5815bea5a9a5f7c1929600c67fc
Marcin <zasadzkimarcin@gmail.com>
2019-11-25 19:55:46 +0000

    skip redux dev tools in createStore

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -3,9 +3,8 @@ import {reducer} from "../reducers";
 import api from "../midleware/api";
 
 export const storeConfig = initState => {
-
   const composeEnhancers = process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : compose;
-  return createStore(reducer, initState, composeEnhancers(applyMiddleware(api)));
+  return createStore(reducer, initState, compose(applyMiddleware(api)));
 };
